89c13fff77e4a98febe0f187913ef0ba6ecf4b59
d1stort <yano4ca@gmail.com>
2017-03-02 20:59:54 +0300

    Latest code for Support Vector Machine

---
+++ text_parse_4_svm.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+#-*- coding: UTF-8 -*-
+import re
+mfile = open('new_sent_vybfin (1).txt','r', encoding='utf-8')
+corpus = []
+for t in mfile:
+ corpus.append(t)
+mfile.close()
+
+spending = []
+deficit = []
+revenue = []
+
+k = 0
+for i in corpus:
+ result = re.search(r'расходы', i) #| result = re.search(r'затраты', i)
+ if result != None:
+   k+=1
+   spending.append(i)
+   result = None
+ else:
+  result = re.search(r'затраты', i)
+  if result != None:
+   k+=1
+   spending.append(i)
+   result = None
+  else:
+   result = re.search(r'траты', i)
+   if result != None:
+    k+=1
+    spending.append(i)
+    result = None
+
+file = open('spndng.txt', 'w')
+file.writelines('%s\n' % i for i in spending)
+file.close()
+
+m = 0
+for i in corpus:
+ result = re.search(r'дефицит', i)
+ if result != None:
+   m+=1
+   deficit.append(i)
+   result = None
+
+file = open('dfct.txt', 'w')
+file.writelines('%s\n' % i for i in deficit)
+file.close()
+ 
+n = 0
+for i in corpus:
+ result = re.search(r'доходы', i)
+ if result != None:
+   n+=1
+   revenue.append(i)
+   result = None   
+
+file = open('rvn.txt', 'w')
+file.writelines('%s\n' % i for i in revenue)
+file.close()
+   
+print('spendings: ',k,' deficit: ',m,' revenue: ',n)
