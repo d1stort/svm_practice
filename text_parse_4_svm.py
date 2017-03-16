c8281080b46b68b65aebc2257a9c29e241603349
d1stort <yano4ca@gmail.com>
2017-03-16 12:01:02 +0300

    Included synonyms for deficit, revenue & spending

--- text_parse_4_svm.py
+++ text_parse_4_svm.py
@@ -13,11 +13,11 @@ revenue = []
 
 k = 0
 for i in corpus:
- result = re.search(r'расходы', i) #| result = re.search(r'затраты', i)
+ result = re.search(r'расходы', i)
  if result != None:
-   k+=1
-   spending.append(i)
-   result = None
+  k+=1
+  spending.append(i)
+  result = None
  else:
   result = re.search(r'затраты', i)
   if result != None:
@@ -30,33 +30,81 @@ for i in corpus:
     k+=1
     spending.append(i)
     result = None
+   else:
+    result = re.search(r'издержки',i)
+    if result != None:
+     k+=1
+     spending.append(i)
+     result = None
 
-file = open('spndng.txt', 'w')
-file.writelines('%s\n' % i for i in spending)
+file = open('spndng.txt', 'w', encoding='utf-8')
+file.writelines('%s/n' % i for i in spending)
 file.close()
 
 m = 0
 for i in corpus:
  result = re.search(r'дефицит', i)
  if result != None:
+  m+=1
+  deficit.append(i)
+  result = None
+ else:
+  result = re.search(r'недостаток', i)
+  if result != None:
    m+=1
    deficit.append(i)
    result = None
+  else:
+   result = re.search(r'нехватка', i)
+   if result != None:
+    m+=1
+    deficit.append(i)
+    result = None
+   else:
+    result = re.search(r'недостача', i)
+    if result != None:
+     m+=1
+     deficit.append(i)
+     result = None
 
-file = open('dfct.txt', 'w')
-file.writelines('%s\n' % i for i in deficit)
+file = open('dfct.txt', 'w', encoding='utf-8')
+file.writelines('%s/n' % i for i in deficit)
 file.close()
  
 n = 0
 for i in corpus:
  result = re.search(r'доходы', i)
  if result != None:
+  n+=1
+  revenue.append(i)
+  result = None
+ else:
+  result = re.search(r'профицит', i)
+  if result != None:
    n+=1
    revenue.append(i)
-   result = None   
+   result = None
+  else:
+   result = re.search(r'профит', i)
+   if result != None:
+    n+=1
+    revenue.append(i)
+    result = None
+   else:
+    result = re.search(r'поступления', i)
+    if result != None:
+     n+=1
+     revenue.append(i)
+     result = None
+    else:
+     result = re.search(r'получения', i)
+     if result != None:
+      n+=1
+      revenue.append(i)
+      result = None
 
-file = open('rvn.txt', 'w')
-file.writelines('%s\n' % i for i in revenue)
+file = open('rvn.txt', 'w', encoding='utf-8')
+file.writelines('%s/n' % i for i in revenue)
 file.close()
    
 print('spendings: ',k,' deficit: ',m,' revenue: ',n)
